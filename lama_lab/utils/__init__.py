2e12c42db54bc71c4040fecb7916fcda10c1d0d5
MatteoOnger <72094799+MatteoOnger@users.noreply.github.com>
2026-08-07 18:53:50 +0200

    Add common.py

--- lama_lab/utils/__init__.py
+++ lama_lab/utils/__init__.py
@@ -1,7 +1,14 @@
 """Utility modules."""
 
 from .buffers import RingBuffer
+from .common import deep_update
 from .logging import setup_logger
 from .storage import ExperimentManager, ResultsManager
 
-__all__ = ["RingBuffer", "setup_logger", "ExperimentManager", "ResultsManager"]
+__all__ = [
+    "RingBuffer",
+    "setup_logger",
+    "deep_update",
+    "ExperimentManager",
+    "ResultsManager",
+]
