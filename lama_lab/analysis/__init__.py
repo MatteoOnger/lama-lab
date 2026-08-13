10834c0e2958cc0dfbfaee2098efdf7de9637acf
MatteoOnger <72094799+MatteoOnger@users.noreply.github.com>
2026-08-13 11:41:29 +0200

    Updating __init__ files

--- lama_lab/analysis/__init__.py
+++ lama_lab/analysis/__init__.py
@@ -3,3 +3,13 @@
 from . import actions
 from . import distributions
 from . import nash
+
+from .actions import compute_action_dispersion
+from .distributions import get_all_unique_fixed_points
+from .nash import get_nash_market_making
+
+__all__ = [
+    "compute_action_dispersion",
+    "get_all_unique_fixed_points",
+    "get_nash_market_making",
+]
--- lama_lab/plotting/__init__.py
+++ lama_lab/plotting/__init__.py
@@ -1,5 +1,9 @@
 """Plotting utilities."""
 
-from . import actions
 from . import distributions
-from . import rewards
+from . import timeseries
+
+from .distributions import plot_1d_histogram, plot_2d_histogram
+from .timeseries import plot_history
+
+__all__ = ["plot_1d_histogram", "plot_2d_histogram", "plot_history"]
