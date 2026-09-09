2e768b314e424734438d6a9110f91c571ff9ae36
MatteoOnger <72094799+MatteoOnger@users.noreply.github.com>
2026-09-09 13:24:21 +0200

    refactor: remove projectors module

--- lama_lab/projectors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Module for projecting values into valid constrained domains."""
-
-from .base import BaseProjector
-from .box import BoxProjector
-from .market_making import MarketMakingProjector
-
-__all__ = ["BaseProjector", "BoxProjector", "MarketMakingProjector"]
